34bec67cb26d34d4d010e14a57e160bbfd504a76
carlosguttemberg <guttembergcarlos@gmail.com>
2020-03-02 22:04:22 -0300

    Adicionando o redis para salvar em cache os dados

--- app/Http/Controllers/ProdutosControlador.php
+++ app/Http/Controllers/ProdutosControlador.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 use App\Produto;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ProdutosControlador extends Controller
 {
     function index(){
-        $produtos = Produto::with('categorias')->get();
+
+        $expiracao = 60; 
+        $produtos = Cache::remember('todosOsProdutos', $expiracao, function() {
+             return Produto::with('categorias')->get();
+        });
+
         return view('produtos', compact(['produtos']));
     }
 }
